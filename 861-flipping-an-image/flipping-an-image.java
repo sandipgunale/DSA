2ea087a5e39a3680ec713ab968014f1769caa427
Sandip Gunale <sandipgunale257@gmail.com>
2025-12-31 22:14:42 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (32.88%) - LeetSync

---
+++ 861-flipping-an-image/flipping-an-image.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        for (int[] row : image) {
+            int left = 0, right = row.length - 1;
+            while (left <= right) {
+                int temp = row[left] ^ 1;
+                row[left] = row[right] ^ 1;
+                row[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        return image;
+    }
+}
