61cb5e237d9d60285e882bf89767243756cdc4ac
Sandip Gunale <sandipgunale257@gmail.com>
2026-04-18 22:15:27 +0530

    Time: 1 ms (100.00%) | Memory: 42.9 MB (15.54%) - LeetSync

--- 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.java
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.java
@@ -2,8 +2,7 @@ class Solution {
     public int reverse(int num){
         int res = 0;
         while(num > 0 ){
-            int dig = num % 10;
-            res = (res * 10) + dig;
+            res = (res * 10) + (num % 10);
             num /= 10;
         }
         return res;
