d6f23b7c4823e1444415ad30930670366e6a76c2
Sandip Gunale <sandipgunale257@gmail.com>
2026-06-27 14:16:00 +0530

    Time: 0 ms (100.00%) | Memory: 80.2 MB (58.17%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -1,13 +1,17 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int peak = 0;
-        for(int i = 0; i < arr.length - 1; i++){
-            if(arr[i] > arr[i + 1]){
-                peak = i;
-                break;
+        int l = 0;
+        int r = arr.length - 1;
+        while(l < r){
+            int mid = l + (r -l) / 2;
+            if(arr[mid] > arr[mid + 1]){
+                r = mid;
+            }
+            else if(arr[mid] < arr[mid + 1]){
+                l = mid + 1;
             }
         }
-        return peak;
+        return l;
     }
     
 }
