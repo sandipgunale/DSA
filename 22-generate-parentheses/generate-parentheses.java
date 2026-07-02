26e0473f95eea686812744f09d45d4c3b664c69d
Sandip Gunale <sandipgunale257@gmail.com>
2026-07-02 12:06:24 +0530

    Time: 2 ms (71.70%) | Memory: 45 MB (13.80%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,8 +1,7 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> list = new LinkedList<>();
-        int o = 0, c = 0;
-        para("", o, c, n, list);
+        List<String> list = new ArrayList<>();
+        para("", 0, 0, n, list);
         return list;
     }
 
