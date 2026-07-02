3f1c65b000fceeadca5b09894c303f87a755294b
Sandip Gunale <sandipgunale257@gmail.com>
2026-07-02 10:10:39 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (22.40%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -8,7 +8,10 @@ class Solution {
      }
     
     private double pow(double x, long n) {
-        if (n == 0) {
+        if (n == 1) {
+            return x;
+        }
+        if(n == 0){
             return 1.0;
         }
         double half = pow(x, n / 2);
