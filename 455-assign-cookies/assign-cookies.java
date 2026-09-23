0096c9fb89adcc82b634d401e459707aac317914
Sandip Gunale <sandipgunale257@gmail.com>
2026-09-23 22:38:15 +0530

    Time: 15 ms (59.73%) | Memory: 51.3 MB (87.21%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -1,20 +1,21 @@
+import java.util.Arrays;
+
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-        int count = 0;
+
         int i = 0;
-        int j = 0;
-        while(i != g.length && j != s.length){
-            if(s[j] >= g[i]){
-                count++;
-                j++;
+
+        for (int cookieSize : s) {
+            if (i == g.length) {
+                break;
+            }
+            if (cookieSize >= g[i]) {
                 i++;
             }
-            else{
-                j++;
-            }
         }
-        return count;
+        
+        return i;
     }
 }
